e1c8e5a1f895f508db6b13065b669de820b696c9
lyb5382 <lyb538202@gmail.com>
2025-08-14 17:01:52 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import { useState, useRef, useEffect } from 'react'
+import { searchWikiTitles } from './api/wiki'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [q, setQ] = useState('')
+  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [err, setErr] = useState('')
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+
+
+  }, [])
+
+  const handleSearch = async () => {
+    const keyword = q.trim()
+
+    if (!keyword) return
+
+    try {
+      setLoading(true)
+      setErr('')
+
+      const pages = await searchWikiTitles(keyword)
+      setItems(pages)
+      setQ('')
+    } catch (error) {
+      console.log(error)
+      setErr("검색중 오류 발생")
+      setItems([])
+    } finally {
+      setLoading(false)
+      inputRef.current?.focus()
+    }
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='app'>
+      <h1>위키백과 검색</h1>
+
+      <div className="search">
+        <input
+          ref={inputRef}
+          value={q}
+          onChange={(e) => setQ(e.target.value)}
+          onKeyUp={(e) => e.key === 'Enter' && handleSearch()}
+          type="text" placeholder='검색어를 입력하세요(예:서울, 한글, 리액트)' />
+        <button onClick={handleSearch} disabled={loading}>
+          {loading ? "검색 중...." : "검색"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {err && <div className="error">에러 코드</div>}
+      <ul className='list'>
+        {items.map((p) => (
+
+          <li key={p.id}>
+            <div>
+              <strong>{p.title}</strong>
+              <p>{p.description || '요약없음'}</p>
+              <a href={`https://ko.wikipedia.org/wiki/${encodeURIComponent(p.title)}`} target='_blank'>위키에서 보기</a>
+            </div>
+          </li>
+        ))}
+
+      </ul>
+    </div>
   )
 }
 
